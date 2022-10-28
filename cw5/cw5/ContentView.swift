4e3ebad11ec363b99c9b64a70c700a348848beff
عائشة القبندي - 389 <aqabandi23@gmail.com>
2022-10-28 11:38:18 +0300

    donee

--- cw5/cw5/ContentView.swift
+++ cw5/cw5/ContentView.swift
@@ -11,49 +11,9 @@ struct ContentView: View {
     @State var counter: [Int] = [0, 0, 0]
     var body: some View {
         VStack{
-            HStack{
-                Text("أستغفر الله العظيم").font(.title)
-                Spacer()
-                Text("\(counter[0])")
-                    .font(.largeTitle)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .foregroundColor(.white)
-                    .background(Color.green)
-                    .clipShape(Circle())
-                    .padding()
-                    .onTapGesture {
-                        counter[0] = counter[0] + 1
-                    }
-            }.padding()
-            HStack{
-                Spacer()
-                Text("الحمدلله").font(.title)
-                Spacer()
-                Text("\(counter[1])")
-                    .font(.largeTitle)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .foregroundColor(.white)
-                    .background(Color.green)
-                    .clipShape(Circle())
-                    .padding()
-                    .onTapGesture {
-                        counter[1] = counter[1] + 1
-                    }
-            }.padding()
-            HStack{
-                Text("سبحان الله وبحمده").font(.title)
-                Spacer()
-                Text("\(counter[2])")
-                    .font(.largeTitle)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .foregroundColor(.white)
-                    .background(Color.green)
-                    .clipShape(Circle())
-                    .padding()
-                    .onTapGesture {
-                        counter[2] = counter[2] + 1
-                    }
-            }.padding()
+            ExtractedView(name:"استغفر الله العظيم", counters: $counter[0])
+            ExtractedView(name:"الحمدلله", counters: $counter[1])
+            ExtractedView(name:"سبحان الله وبحمده", counters: $counter[2])
         }
     }
 }
@@ -64,5 +24,26 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ExtractedView: View {
+    var name: String
+    @Binding var counters: Int
+    var body: some View {
+        HStack{
+            Text(name).font(.title)
+            Spacer()
+            Text("\(counters)")
+                .font(.largeTitle)
+                .frame(width: 100, height: 100, alignment: .center)
+                .foregroundColor(.white)
+                .background(Color.green)
+                .clipShape(Circle())
+                .padding()
+                .onTapGesture {
+                    counters += 1
+                }
+        }.padding()
     }
 }
